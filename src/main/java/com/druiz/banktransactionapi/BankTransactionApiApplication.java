2de8e7aa55151e2583c5c4d849903f5725614822
druiz35 <escobarruizd@gmail.com>
2026-02-11 09:46:38 -0500

    Initial commit

---
+++ src/main/java/com/druiz/banktransactionapi/BankTransactionApiApplication.java
@@ -0,0 +1,13 @@
+package com.druiz.banktransactionapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankTransactionApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankTransactionApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/druiz/banktransactionapi/BankTransactionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.druiz.banktransactionapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankTransactionApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
