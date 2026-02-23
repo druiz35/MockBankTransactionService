abf0a5e48fc47ea7a5bd78c189f1c15040278c2c
druiz35 <escobarruizd@gmail.com>
2026-02-23 13:36:41 -0500

    fix: Changed Transaction entity types to bytes and String for proper DynamoDB handling

--- src/main/java/com/druiz/mockbanktransactionservice/model/Transaction.java
+++ src/main/java/com/druiz/mockbanktransactionservice/model/Transaction.java
@@ -1,33 +1,40 @@
 package com.druiz.mockbanktransactionservice.model;
 
-import com.druiz.mockbanktransactionservice.enums.ProductTypeEnum;
-import com.druiz.mockbanktransactionservice.enums.TransactionTypeEnum;
+//import com.druiz.mockbanktransactionservice.enums.ProductTypeEnum;
+//import com.druiz.mockbanktransactionservice.enums.TransactionTypeEnum;
 import lombok.Getter;
 import lombok.Setter;
 import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
 import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
 
-import java.time.Instant;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
-@DynamoDbBean
 @Getter
 @Setter
+@DynamoDbBean
 public class Transaction {
-    private UUID id;
-    private UUID originProductID;
-    private ProductTypeEnum originProductType;
-    private UUID destinationProductID;
-    private ProductTypeEnum destinationProductType;
-    private Instant timestamp;
+    private byte[] id;
+    private byte[] originProduct;
+    //private ProductTypeEnum originProductType;
+    private String originProductType;
+    private byte[] destinationProduct;
+    //private ProductTypeEnum destinationProductType;
+    private String destinationProductType;
+    private LocalDateTime dateTime;
     private float amountWithoutAppliedFee;
     private float amountWithAppliedFee;
     private float appliedFee;
-    private TransactionTypeEnum type;
+    //private TransactionTypeEnum type;
+    private String type;
 
     @DynamoDbPartitionKey
-    public UUID getId() {
+    public byte[] getId() {
         return id;
+    }
+
+    public void setIdFromUUID(UUID uuid) {
+        this.id = uuid.toString().getBytes();
     }
 
 }
